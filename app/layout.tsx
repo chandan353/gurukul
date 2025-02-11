72c99c740a7f59ca4e22175704467e90563f916a
chandan353 <chandanblp2000@gmail.com>
2025-02-11 15:43:13 +0530

    improved seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
@@ -10,12 +11,12 @@ import "slick-carousel/slick/slick-theme.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Gurukul Academy Balrampur",
+	title: "Gurukul Academy",
 	description: "Nurturing minds, shaping futures - Gurukul Academy Balrampur",
 	openGraph: {
-		title: "Gurukul Academy Balrampur",
+		title: "Gurukul Academyr",
 		description:
-			"Nurturing minds, shaping futures - Gurukul Academy  Balrampur",
+			"Nurturing minds, shaping futures - Gurukul Academy Balrampur",
 		url: "https://gurukulacademybalrampur.in",
 		siteName: "Gurukul Academy",
 		images: [
@@ -38,6 +39,20 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" className="scroll-smooth">
+			<Head>
+				<meta name="geo.region" content="IN-UP" />
+				<meta name="geo.placename" content="Balrampur" />
+				<meta name="geo.position" content="27.4020846;82.2030742" />
+				<meta name="ICBM" content="27.4020846, 82.2030742" />
+				<meta
+					property="og:see_also"
+					content="https://facebook.com/people/Gurukul-Academy-Powered-by-Lead"
+				/>
+				<meta
+					property="og:see_also"
+					content="https://www.instagram.com/gurukul_academy_by_lead"
+				/>
+			</Head>
 			<body className={inter.className}>
 				<Navbar />
 				<main className="min-h-screen">{children}</main>
