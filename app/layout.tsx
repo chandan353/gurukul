9fd8545d8f30e6ba7f0e1fdd658b0baa0995da2a
chandan353 <chandanblp2000@gmail.com>
2025-02-11 14:33:40 +0530

    reel fix and added meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,25 @@ import "slick-carousel/slick/slick-theme.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Gurukul Academy",
-	description: "Nurturing minds, shaping futures - Gurukul Academy",
+	title: "Gurukul Academy Balrampur",
+	description: "Nurturing minds, shaping futures - Gurukul Academy Balrampur",
+	openGraph: {
+		title: "Gurukul Academy Balrampur",
+		description:
+			"Nurturing minds, shaping futures - Gurukul Academy  Balrampur",
+		url: "https://gurukulacademybalrampur.in",
+		siteName: "Gurukul Academy",
+		images: [
+			{
+				url: "https://gurukulacademybalrampur.in/_next/static/media/school1.3f9fc10a.jpg",
+				width: 800,
+				height: 600,
+				alt: "Gurukul Academy",
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
 };
 
 export default function RootLayout({
